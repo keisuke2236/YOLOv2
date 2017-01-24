9de7977b343138be4f7370328709f26adee9d461
Ubuntu <ubuntu@ip-172-31-42-213.us-west-2.compute.internal>
2017-01-24 10:25:14 +0000

    add iou utils for gpu

--- lib/utils.py
+++ lib/utils.py
@@ -1,3 +1,7 @@
+import chainer.functions as F
+import numpy as np
+from chainer import Variable
+
 def print_cnn_info(name, link, shape_before, shape_after, time):
     n_stride = (
         int((shape_before[2] + link.pad[0] * 2 - link.ksize) / link.stride[0]) + 1,
@@ -54,12 +58,35 @@ def overlap(x1, len1, x2, len2):
 
     return right - left
 
+# chainerのVariable用のoverlap
+def multi_overlap(x1, len1, x2, len2):
+    len1_half = len1/2
+    len2_half = len2/2
+
+    left = F.maximum(x1 - len1_half, x2 - len2_half)
+    right = F.minimum(x1 + len1_half, x2 + len2_half)
+
+    return right - left
+
 # 2つのboxを受け取り、被ってる面積を返す(intersection of 2 boxes)
 def box_intersection(a, b):
     w = overlap(a.x, a.w, b.x, b.w)
-    h = overlap(a.x, a.h, b.x, b.h)
+    h = overlap(a.y, a.h, b.y, b.h)
     if w < 0 or h < 0:
         return 0
+
+    area = w * h
+    return area
+
+# chainer用
+def multi_box_intersection(a, b):
+    w = multi_overlap(a.x, a.w, b.x, b.w)
+    h = multi_overlap(a.y, a.h, b.y, b.h)
+    zeros = Variable(np.zeros(w.shape, dtype=w.data.dtype))
+    zeros.to_gpu()
+
+    w = F.maximum(w, zeros)
+    h = F.maximum(h, zeros)
 
     area = w * h
     return area
@@ -70,6 +97,16 @@ def box_union(a, b):
     u = a.w * a.h + b.w * b.h - i
     return u
 
+# chianer用
+def multi_box_union(a, b):
+    i = multi_box_intersection(a, b)
+    u = a.w * a.h + b.w * b.h - i
+    return u
+
 # compute iou
 def box_iou(a, b):
     return box_intersection(a, b) / box_union(a, b)
+
+# chainer用
+def multi_box_iou(a, b):
+    return multi_box_intersection(a, b) / multi_box_union(a, b)
