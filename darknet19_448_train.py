a29da9766d5ce24141162607461d690322d4ff7a
Ubuntu <ubuntu@ip-172-31-42-213.us-west-2.compute.internal>
2017-01-31 09:56:00 +0000

    darknet19とdarknet19_448の修正

--- darknet19_448_train.py
+++ darknet19_448_train.py
@@ -7,11 +7,14 @@ import os
 from chainer import serializers, optimizers, Variable, cuda
 import chainer.functions as F
 from darknet19 import *
+from lib.image_generator import *
 
 # hyper parameters
 input_height, input_width = (448, 448)
-train_file = "../dataset/fruits_dataset/train.txt"
-label_file = "../dataset/fruits_dataset/label.txt"
+item_path = "./items"
+background_path = "./backgrounds"
+label_file = "./data/label.txt"
+initial_weights_file = "backup/darknet19_final.model"
 backup_path = "backup"
 batch_size = 16
 max_batches = 3000
@@ -20,44 +23,23 @@ lr_decay_power = 4
 momentum = 0.9
 weight_decay = 0.0005
 
-# load dataset
-with open(train_file, "r") as f:
-    image_files = f.read().strip().split("\n")
+# load image generator
+print("loading image generator...")
+generator = ImageGenerator(item_path, background_path)
 
 with open(label_file, "r") as f:
     labels = f.read().strip().split("\n")
-
-x_train = []
-t_train = [] # normal label
-t_train_one_hot = [] # one hot label
-print("loading image datasets...")
-for image_file in image_files:
-    img = cv2.imread(image_file)
-    img = cv2.resize(img, (input_height, input_width))
-    img = np.asarray(img, dtype=np.float32) / 255.0
-    img = img.transpose(2, 0, 1)
-    x_train.append(img)
-    t_train_one_hot.append(np.zeros(len(labels)))
-    for i, label in enumerate(labels):
-        if label in image_file:
-            t_train.append(i)
-            t_train_one_hot[-1][i] = 1
-x_train = np.array(x_train)
-t_train = np.array(t_train, dtype=np.int32)
-t_train_one_hot = np.array(t_train_one_hot, dtype=np.float32)
 
 # load model
 print("loading model...")
 model = Darknet19Predictor(Darknet19())
 backup_file = "%s/backup.model" % (backup_path)
 if os.path.isfile(backup_file):
-    serializers.load_hdf5(backup_file, model) # load saved model
+    serializers.load_hdf5(initial_weights_file, model) # load saved model
 model.predictor.train = True
 model.predictor.finetune = True
-
-if hasattr(cuda, "cupy"):
-    cuda.get_device(0).use()
-    model.to_gpu() # for gpu
+cuda.get_device(0).use()
+model.to_gpu() # for gpu
 
 optimizer = optimizers.MomentumSGD(lr=learning_rate, momentum=momentum)
 optimizer.use_cleargrads()
@@ -68,15 +50,30 @@ optimizer.add_hook(chainer.optimizer.WeightDecay(weight_decay))
 # start to train
 print("start training")
 for batch in range(max_batches):
-    batch_mask = np.random.choice(len(x_train), batch_size)
-    x = Variable(x_train[batch_mask])
-    #t = Variable(t_train[batch_mask]) # use normal label and softmax_cross_entropy
-    t = Variable(t_train_one_hot[batch_mask]) # use one hot label and squared error
-    if hasattr(cuda, "cupy"):
-        x.to_gpu() # for gpu
-        t.to_gpu() # for gpu
+    # generate sample
+    x, t = generator.generate_samples(
+        n_samples=batch_size,
+        n_items=1,
+        crop_width=input_width,
+        crop_height=input_height,
+        min_item_scale=1,
+        max_item_scale=3,
+        rand_angle=15,
+        minimum_crop=0.8,
+        delta_hue=0.01,
+        delta_sat_scale=0.5,
+        delta_val_scale=0.5
+    )
+    x = Variable(x)
+    one_hot_t = []
+    for i in range(len(t)):
+        one_hot_t.append(t[i][0]["one_hot_label"])
+    x.to_gpu()
+    one_hot_t = np.array(one_hot_t, dtype=np.float32)
+    one_hot_t = Variable(one_hot_t)
+    one_hot_t.to_gpu()
 
-    y, loss, accuracy = model(x, t)
+    y, loss, accuracy = model(x, one_hot_t)
     print("[batch %d (%d images)] learning rate: %f, loss: %f, accuracy: %f" % (batch+1, (batch+1) * batch_size, optimizer.lr, loss.data, accuracy.data))
 
     optimizer.zero_grads()
